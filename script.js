1206e20d9ae1ce0e4eea0a5d8b939e9758f9aae5
tofiw13 <nuritofiq12@gmail.com>
2026-02-09 21:57:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
